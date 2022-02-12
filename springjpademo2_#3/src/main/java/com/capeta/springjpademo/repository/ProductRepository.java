dde536960de26fcea356eacb142462efa90d9cb6
sriramcapeta <35570305+sriramcapeta@users.noreply.github.com>
2022-02-12 19:57:04 +0530

    Delete springjpademo2_#3 directory

--- springjpademo2_#3/src/main/java/com/capeta/springjpademo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capeta.springjpademo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.capeta.springjpademo.entity.Product;
-
-public interface ProductRepository extends CrudRepository<Product,Long> {
-
-}
